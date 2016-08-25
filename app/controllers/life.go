882cb7ce5af578eceb30e17cffc65b73981d8c8d
Kyle Richardson <kylerchrdsn@gmail.com>
2016-08-24 19:12:57 -0500

    @.@

--- app/controllers/life.go
+++ app/controllers/life.go
@@ -10,8 +10,8 @@ type Life struct {
 func (c Life) Index() revel.Result {
 	var rows, cols [12]int
 	for i := 0; i < 12; i++ {
-		rows[i] = i + 1
-		cols[i] = i + 1
+		rows[i] = i
+		cols[i] = i
 	}
 	return c.Render(rows, cols)
 }
